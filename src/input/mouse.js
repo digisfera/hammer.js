1cf8c89169a62522a2c71a5a6118f6183505a368
Manuel Cabral <m.cabral@digisfera.pt>
2014-11-13 11:20:14 +0000

    Handle IE8 mouse events

--- src/input/mouse.js
+++ src/input/mouse.js
@@ -7,6 +7,8 @@ var MOUSE_INPUT_MAP = {
 var MOUSE_ELEMENT_EVENTS = 'mousedown';
 var MOUSE_WINDOW_EVENTS = 'mousemove mouseup';
 
+var isIE8 = window.navigator.userAgent.indexOf('MSIE 8') > 0;
+
 /**
  * Mouse events input
  * @constructor
@@ -14,7 +16,13 @@ var MOUSE_WINDOW_EVENTS = 'mousemove mouseup';
  */
 function MouseInput() {
     this.evEl = MOUSE_ELEMENT_EVENTS;
-    this.evWin = MOUSE_WINDOW_EVENTS;
+
+    if (isIE8) {
+        // mousemove and moveup don't bubble to the window in IE8 - attach events to the document.body instead
+        this.evDoc = MOUSE_WINDOW_EVENTS;
+    } else {
+        this.evWin = MOUSE_WINDOW_EVENTS;
+    }
 
     this.allow = true; // used by Input.TouchMouse to disable mouse events
     this.pressed = false; // mousedown state
@@ -30,12 +38,24 @@ inherit(MouseInput, Input, {
     handler: function MEhandler(ev) {
         var eventType = MOUSE_INPUT_MAP[ev.type];
 
+        // IE8 uses non-standard button indices:
+        // http://msdn.microsoft.com/en-us/library/ie/ms533544(v=vs.85).aspx
+        // left button is 1
+        //
+        // Standard is here:
+        // http://msdn.microsoft.com/en-us/library/ie/ff974877(v=vs.85).aspx
+        // left button is 0
+        var leftMouseButton = 0;
+        if (isIE8) {
+            leftMouseButton = 1;
+        }
+
         // on start we want to have the left mouse button down
-        if (eventType & INPUT_START && ev.button === 0) {
+        if (eventType & INPUT_START && ev.button === leftMouseButton) {
             this.pressed = true;
         }
 
-        if (eventType & INPUT_MOVE && ev.which !== 1) {
+        if (eventType & INPUT_MOVE && ev.button !== leftMouseButton) {
             eventType = INPUT_END;
         }
 
